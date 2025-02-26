1d9435074d590e42fdb4cb48f702827f33900e45
DonShanilka <shanilka799@gmail.com>
2025-02-26 13:29:50 +0530

    create store

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import eventSlice from '../reducer/EventSlice'
 
 export const store = configureStore({
     reducer: {
-        event: 
+        event: eventSlice
     }
 })
+
+export default store;
